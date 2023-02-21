320db5ae49f89537a0d3a3464cee64ec9ff5185b
Garden <66628342+ljw9611@users.noreply.github.com>
2023-02-21 22:49:08 +0900

    01. UseAuthentication 확장 메서드를 사용하여 인증된 사용자의 정보 가져오기

--- AuthenticationAuthorization2/Program.cs
+++ AuthenticationAuthorization2/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
+using System.Text;
 
 namespace AuthenticationAuthorization2
 {
@@ -9,9 +10,9 @@ namespace AuthenticationAuthorization2
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddAuthentication("Cookies").AddCookie();
-
-            var app = builder.Build();
             
+            var app = builder.Build();
+            app.UseAuthentication();
 
             app.MapGet("/", async context => {
                 string content = "<h1>ASP .NET Core 인증과 권한 초간단 코드</h1>";
@@ -26,7 +27,7 @@ namespace AuthenticationAuthorization2
 
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimTypes.Name, "User Name")
+                    new Claim(ClaimTypes.Name, "User Name") //
                 };
 
                 var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
@@ -38,7 +39,21 @@ namespace AuthenticationAuthorization2
                 context.Response.Headers.ContentType = "text/html; charset=utf-8";
                 await context.Response.WriteAsync("<h3>로그인 완료</h3>");
             });
+            app.MapGet("/Info", async context =>
+            {
+                string result = "";
 
+                if (context.User.Identity.IsAuthenticated)
+                {   // User Name을 가져옴 (30 Line)
+                    result += $"<h3>로그인 이름: {context.User.Identity.Name}</h3>";
+                }
+                else
+                {
+                    result += "<h3>로그인하지 않았습니다.<h3>";
+                }
+                context.Response.Headers.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync(result, Encoding.Default);
+            });
             app.Run();
         }
     }
